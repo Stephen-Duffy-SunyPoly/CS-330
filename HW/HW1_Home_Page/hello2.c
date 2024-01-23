8f4c9bbbed3ae50dd61651dff4e1bb93f858f445
Stephen <duffysd@sunypoly.edu>
2024-01-23 16:09:06 -0500

    ammendment to HW 1

---
+++ HW/HW1_Home_Page/hello2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>       //Pre-processor directive
+#include <sys/time.h>
+struct timeval tv;
+void main()             //main function declaration
+{
+    int x= gettimeofday(&tv, 0);
+    printf("Hello World %d", (int)tv.tv_usec);      //to output the string on a display
+    getchar ();             //terminating function
+    extern FILE* stdin;
+    getc (stdin);           //terminating function
+}
